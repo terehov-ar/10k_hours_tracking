4430ae4f17b7ac988c4ba5b8c1186abdb653137a
tereh0v <43907782+tereh0v@users.noreply.github.com>
2022-02-10 14:05:37 +0300

    Create a data structure (step 4)

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const button = document.getElementById("todoListButton");
+const textArray = [];
 
 button.addEventListener("click", () => {
-    const taskInput = document.getElementById("textInput").value;
+    let inputElement = document.getElementById("textInput");
+    const taskInput = inputElement.value;
     let listItem;
 
     if (taskInput === "") {
@@ -10,13 +12,23 @@ button.addEventListener("click", () => {
     else {
         listItem = document.createElement("li");
         const text = document.createTextNode(taskInput);
-        listItem.appendChild(text);
-        document.querySelectorAll('input').forEach(input => input.value='')
+        listItem.append(text); 
+        const today = new Date(); 
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        const uuid = self.crypto.randomUUID();
+        const textObject = {
+            id: uuid,
+            text: taskInput,
+            createdAt: date + " : " + time,
+        };
+        textArray.push(textObject);
+        inputElement.value = '';
     }
     if (!listItem) {
         return;
     }
     const list = document.getElementById("listOfTodos");
-    list.appendChild(listItem);
+    list.append(listItem);
 
 });
